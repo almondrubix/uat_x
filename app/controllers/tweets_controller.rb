db4972a3247069cb64f7396895cee47a8b45e4ca
almondrubix <allen2rubin@gmail.com>
2025-02-15 16:41:23 -0600

    created create def for creating tweets and created first tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,14 @@ class TweetsController < ApplicationController
 	def new
 		@tweet = Tweet.new
 	end
+
+	def create
+		print params["tweet"]["content"]
+		@tweet = Tweet.new
+		@tweet.content = params["tweet"]["content"]
+		@tweet.save
+
+		redirect_to new_tweet_path
+		#this is the code that wil create a new tweet
+	end
 end
